8b4d69cd1b42519858026bea9c026c878a9feb87
93geraldtee <93geraldtee@gmail.com>
2020-07-10 15:47:08 -0400

    added controller actions

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::RestaurantsController < ApplicationController
+    before_action :set_restaurant, only: [:show, :update, :destroy]
 
     def index
         @restaurants = Restaurant.all
@@ -7,18 +8,35 @@ class Api::V1::RestaurantsController < ApplicationController
     end
 
     def create
-
+        @@restaurant = Restaurant.new(restaurant_params)
+        if @restaurant.save
+            render json: @restaurant
+        else    
+            render json: {error: @restaurant.errors.full_messages}, status: :unprocessable_entity
+        end
     end
 
     def show
+        render json: @restaurant
+    end
 
+    def update
+        if @restaurant.update(restaurant_params)
+            render json: @restaurant
+        else
+            render json: @restaurant.errors, status: :unprocessable_entity
+        end
     end
 
     def destroy
-
+        @restaurant.destroy
     end
 
     private
+
+    def set_restaurant
+        @restaurant = Restaurant.find(params[:id])
+    end
 
     def restaurant_params
         params.require(:restaurant).permit(:name, :location)
