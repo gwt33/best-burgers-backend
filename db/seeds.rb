d4da5781264a29687281be0194608ede4c4ee3da
93geraldtee <93geraldtee@gmail.com>
2020-07-10 00:18:55 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+restaurant = Restaurant.create(name: "Zinburger", location: "Paramus, NJ")
+restaurant_two = Restaurant.create(name: "Bobby's Burger Palace", location: "Paramus, NJ")
+
+burger = Burger.create(restaurant_id: 1, name: "Zinburger", description: "Manchego Cheese, Zinfandel Braised Onions, Lettuce & Mayo", price: 12.00)
+burger_two = Burger.create(restaurant_id: 1, name: "Samburger", description: "Applewood Smoked Bacon, American Cheese, Lettuce & 1,000 Island", price: 12.50)
+
+burger_three = Burger.create(restaurant_id: 2, name: "Bobby Blue Bacon", description: "Blue cheese, bacon, lettuce, tomato", price: 9.75)
+burger_four = Burger.create(restaurant_id: 2, name: "L.A.", description: "Avocado relish, watercress, cheddar cheese, tomato", price: 9.50)
