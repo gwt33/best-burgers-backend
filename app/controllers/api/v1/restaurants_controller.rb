93b232972b4796d96219e3b3c6de1d3c75498705
93geraldtee <93geraldtee@gmail.com>
2020-07-10 15:33:36 -0400

    add controller actions

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::RestaurantsController < ApplicationController
+
+    def index
+        @restaurants = Restaurant.all
+
+        render json: @restaurants, include: :burgers
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def restaurant_params
+        params.require(:restaurant).permit(:name, :location)
+    end
+
 end
