eedf4897601d2babca858250c92565f10eac16b7
93geraldtee <93geraldtee@gmail.com>
2020-07-13 11:31:58 -0400

    update create method

--- app/controllers/api/v1/burgers_controller.rb
+++ app/controllers/api/v1/burgers_controller.rb
@@ -8,9 +8,9 @@ class Api::V1::BurgersController < ApplicationController
     end
 
     def create
-        @burger = Burger.new(burger_params)
+        @burger = @restaurant.burgers.new(burger_params)
         if @burger.save
-            render json: @burger
+            render json: @restaurant
         else    
             render json: {error: @burger.errors.full_messages}, status: :unprocessable_entity
         end
